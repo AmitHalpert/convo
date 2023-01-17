07c15a6702d0c230c64136781a35db69d7f29ea6
amit <amit.ethan.halpert@gmail.com>
2023-01-17 17:47:13 +0200

    pyproject: added start method, fixed project for building and running

--- convo/main/Main.py
+++ convo/main/Main.py
@@ -4,51 +4,53 @@ import numpy as np
 import math
 import time
 
-cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
 
-offset = 20
-imgSize = 300
+def start():
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector(maxHands=1)
 
-folder = "Data"
-counter = 0
+    offset = 20
+    imgSize = 300
 
-while True:
-    success, img = cap.read()
-    hands, img = detector.findHands(img)
-    if hands:
-        hand = hands[0]
-        x, y, w, h = hand['bbox']
+    folder = "Data"
+    counter = 0
 
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
-        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+    while True:
+        success, img = cap.read()
+        hands, img = detector.findHands(img)
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand['bbox']
 
-        imgCropShape = imgCrop.shape
+            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+            imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
 
-        aspectRatio = h / w
+            imgCropShape = imgCrop.shape
 
-        if aspectRatio > 1:
-            k = imgSize / h
-            wCal = math.ceil(k * w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            imgResizeShape = imgResize.shape
-            wGap = math.ceil((imgSize - wCal) / 2)
-            imgWhite[:, wGap:wCal + wGap] = imgResize
+            aspectRatio = h / w
 
-        else:
-            k = imgSize / w
-            hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-            imgResizeShape = imgResize.shape
-            hGap = math.ceil((imgSize - hCal) / 2)
-            imgWhite[hGap:hCal + hGap, :] = imgResize
+            if aspectRatio > 1:
+                k = imgSize / h
+                wCal = math.ceil(k * w)
+                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                imgResizeShape = imgResize.shape
+                wGap = math.ceil((imgSize - wCal) / 2)
+                imgWhite[:, wGap:wCal + wGap] = imgResize
 
-        cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite", imgWhite)
+            else:
+                k = imgSize / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                imgResizeShape = imgResize.shape
+                hGap = math.ceil((imgSize - hCal) / 2)
+                imgWhite[hGap:hCal + hGap, :] = imgResize
 
-    cv2.imshow("Image", img)
-    key = cv2.waitKey(1)
-    if key == ord("s"):
-        counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
-        print(counter)
+            cv2.imshow("ImageCrop", imgCrop)
+            cv2.imshow("ImageWhite", imgWhite)
+
+        cv2.imshow("Image", img)
+        key = cv2.waitKey(1)
+        if key == ord("s"):
+            counter += 1
+            cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+            print(counter)
